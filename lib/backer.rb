34e7b230edc9132c82d4121771e02b9b90b290bd
Crystal Norman <cnorm88@gmail.com>
2020-10-10 15:50:50 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+  
+  attr_reader :name
+  attr_accessor :backed_projects 
+  
+  def initialize(name) 
+    @name = name
+    @backed_projects = []
+  end
+  
+  def backed_projects
+    self.backed_projects << project
+  end
+  
+end
